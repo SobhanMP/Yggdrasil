410f41cb2a43bdc42e0b65e2d74b5559f6880e57
Sobhan Mohammadpour <www.sobhan.mohammadpour@gmail.com>
2022-07-19 13:48:54 -0400

    WIP: bump nomad_jll

--- N/NOMAD/build_tarballs.jl
+++ N/NOMAD/build_tarballs.jl
@@ -1,25 +1,23 @@
 using BinaryBuilder
 
 name = "NOMAD"
-version = v"4.0.0"
+version = v"4.2.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/amontoison/nomad.git","a15c91feb589451e6934b1544edb4360af8fbc41"),
-    DirectorySource("./bundled"),
+    GitSource("https://github.com/bbopt/nomad.git","02ffde35e70441f9d275f17f952ffc86956c57c1"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd "${WORKSPACE}/srcdir/nomad"
-atomic_patch -p1 "${WORKSPACE}/srcdir/patches/nomad-no-headers-generation.patch"
-if [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-freebsd* ]]; then
-    CC=gcc
-    CXX=g++
-fi
+mkdir "${WORKSPACE}/path"
+export PATH="${WORKSPACE}/path:$PATH}"
 mkdir build
 cd build
-cmake -DNOMAD_WITH_OPENMP=OFF -DNOMAD_WITH_EXAMPLES=OFF -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release ..
+sed s/'set(CMAKE_INSTALL_PREFIX ${PROJECT_BINARY_DIR} CACHE PATH "..." FORCE)'// -i ../CMakeLists.txt
+g++ ${WORKSPACE}/srcdir/nomad/src/Attribute/WriteAttributeDefinitionFile.cpp -o ${WORKSPACE}/path/WriteAttributeDefinitionFile
+cmake -DBUILD_INTERFACE_C=ON -DTEST_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
 """
@@ -27,10 +25,9 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
-
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libnomadInterface", :libnomadInterface),
+    LibraryProduct("libnomadCInterface", :libnomadCInterface),
     LibraryProduct("libnomadAlgos", :libnomadAlgos),
     LibraryProduct("libnomadEval", :libnomadEval),
     LibraryProduct("libnomadUtils", :libnomadUtils),
@@ -40,8 +37,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CompilerSupportLibraries_jll"),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", 
+    uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
-
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"12", julia_compat="v1.8")
